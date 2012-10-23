187b90d1a8676adcee1956a55575e2284073195e
Alexis Smirnov <alexis@smirnov.ca>
2012-10-22 21:42:32 -0400

    https port fix 443

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,11 @@
 var https = require('https');
-
+var querystring = require('querystring');
+var longjohn = require('longjohn');
 
 var media_root = 'https://radialpoint.kindlingapp.com';
 var kindling_shared_secret = process.env.KINDLING_SHARED_SECRET;
 var kindling_api_endpoint = 'radialpoint.kindlingapp.com';
-var kindling_api_port = 80;
-
-var kindling_api_headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-};
+var kindling_api_port = 443;
 
 exports.login = function(req, res){
   res.render('login', { 
@@ -17,8 +14,7 @@ exports.login = function(req, res){
 };
 
 exports.submit_login = function(req, res){
-	console.log(req.body.email);
-	console.log(req.body.password);
+	//res.render('index', { title: 'Express' });
 
 	// 1. authenticate
 
@@ -31,19 +27,47 @@ exports.submit_login = function(req, res){
 	// expires(optional, defaults to 3 hours. for full allowed time formats see here)=+1 day
 	// pattern based on 
 	// http://nodejs.org/docs/v0.4.11/api/http.html#http.request
-	data = JSON.stringify({ 'user': req.body.email, 'secret': kindling_shared_secret });
-	headers = kindling_api_headers;
-	headers['Content-Length'] = Buffer.byteLength(data,'utf8');
+	data = querystring.stringify({ 
+		'user': req.body.email, 
+		'secret': kindling_shared_secret 
+	});
+
 	options = {
 		host: kindling_api_endpoint,
 		port: kindling_api_port,
 		path: '/api/authenticator',
 		method: 'POST',
-		headers: headers
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Content-Length': data.length	
+		}
 	};
 
 
-	var request = https.request(options);
+	var request = https.request(options, function(response) {
+		response.setEncoding('utf8');
+
+		response.on('data', function(chunk) {
+			console.log('received from kindling:');
+			console.log(chunk);
+
+
+			req.method = 'get';
+			res.redirect('http://yahoo.com');
+		});
+		response.on('end', function(chunk) {
+			console.log('end received');
+		});
+	});
+
+	request.on('socket', function(socket) {
+		socket.on('connect', function () {
+			console.log('socket connected!');
+		});
+		socket.on('close', function () {
+			console.log('socket closed!');
+		});
+	});
 
 	request.on('response', function(response) {
 		response.on('data', function(chunk) {
@@ -59,15 +83,14 @@ exports.submit_login = function(req, res){
 		});
 	});
 	
-	req.on('error', function(e) {
-		console.log('problem with request: ' + e.message);
-	});
+	// req.on('error', function(e) {
+	// 	console.log('problem with request: ' + e.message);
+	// });
 
 	request.write(data);
-	request.end();
+	//request.end();
 
-	console.log('wrote this request');
-	console.log(request);
+	console.log('wrote request');
+	//console.log(request);
 	// 3. redirect
 };
-
