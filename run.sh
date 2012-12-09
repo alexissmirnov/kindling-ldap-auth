7a9b35bd5bff036f67234534f1cc19b9e1211352
Alexis Smirnov <alexis@smirnov.ca>
2012-12-09 11:22:13 -0500

    npm install

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 # export KLA_KINDLING_SHARED_SECRET=""
 
+npm install
 export KLA_KINDLING_ACCOUNT_ENDPOINT="radialpoint.kindlingapp.com"
 export KLA_LDAP_SERVER_URL = "ldaps://yul01wdc02.rp.corp:636"
 export KLA_LDAP_BASE = "OU=Users,OU=Managed Objects,DC=rp,DC=corp"
